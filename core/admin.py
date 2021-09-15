486563b728f9b1dd8d9b3ecc9dcef359656983d6
CosmoLustosa <cosmolustosa2@gmail.com>
2021-09-15 16:49:38 -0300

    pasta core atualizada

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,7 @@ from core.models import Evento
 # Register your models here.
 
 class EventoAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'descricao', 'data_evento', 'data_criacao') #lista os atributos a serem mostrados no admin
+    list_display = ('id', 'titulo', 'descricao', 'data_evento', 'data_criacao') #lista os atributos a serem mostrados no admin
     list_filter = ('titulo', 'data_evento') #adiciona um filtro no admin do django
 
 admin.site.register(Evento, EventoAdmin) #Associa as classes
